5355b06ee9c81d4d91a83d7dacfaf99919830ec4
sahaavik2014-byte <saha.avik2014@gmail.com>
2026-07-01 09:38:53 -0700

    Add files via upload

---
+++ TypeCasting.py
@@ -0,0 +1,7 @@
+myAge = 12
+print("Type of data before casting" ,type(myAge))
+newAge = float(myAge)
+print("Type of data before casting" ,type(newAge))
+isstudying = True
+newvariable = str(isstudying)
+print("Type of newvariable" ,type(newvariable))
