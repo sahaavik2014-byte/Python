542a4302ccd71b476e81e4dc4b5c5f913bcaea31
sahaavik2014-byte <saha.avik2014@gmail.com>
2026-07-01 09:54:54 -0700

    Add files via upload

---
+++ StringOperation.py
@@ -0,0 +1,6 @@
+FavoriteFootballer = input ("Who is your favorite footballer ")
+print("Okay you have entered ", FavoriteFootballer)
+
+Name = input ("What is your name ")
+reversedname = Name[::-1]
+print("Your name reversed is",reversedname)
