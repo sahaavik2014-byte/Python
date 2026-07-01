12b78a5aec98cd637e961efd46b368a9c207e281
sahaavik2014-byte <saha.avik2014@gmail.com>
2026-07-01 09:27:09 -0700

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,8 @@
+name = "Avik Saha"
+print("Type of name is" ,type(name))
+age = 12
+print("Type of age is" ,type(age))
+weight = 95.67
+print("The type of weight is" ,type(weight))
+RonaldoIsTheGoat = False
+print("The type of is Male" ,type(RonaldoIsTheGoat))
